b2ee587978162fcbf19b34fbfdfed9ca504b41ed
Medetcan Diler <medetdiler1996@gmail.com>
2023-11-13 18:24:40 +0300

    Implemented mongodb connection to the backend

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log(req);
+  return res.status(234).send('Welcome to msdfsdy fullstack application')
+});
+
+
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('Connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    })
+  })
+  .catch( err => {
+    console.log(err.message)
+  })
