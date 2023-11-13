a498cd433f0632952d75bf1a32eac76c7db57c6e
Medetcan Diler <medetdiler1996@gmail.com>
2023-11-13 20:57:53 +0300

    Implemented cors policy

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,17 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from './routes/booksRoute.js'
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+  origin: '',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+}));
 
 app.get('/', (request, response) => {
   console.log(request);
@@ -13,6 +20,7 @@ app.get('/', (request, response) => {
 });
 
 app.use('/books', booksRoute);
+
 
 mongoose
   .connect(mongoDBURL)
